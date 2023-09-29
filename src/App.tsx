d6b86d6207cdf5e680d7062faa626afa462ea494
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-09-29 19:08:33 +0530

    create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Test from './pages/Test';
-import Finish from './pages/Finish';
-import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
+import Test from "./pages/Test";
+import Finish from "./pages/Finish";
+import Home from "./pages/Home";
+import "./App.css";
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/test' element={<Test/>} />
-            <Route path='/finish' element={<Finish/>} />
-        </Routes>
-    );
-}
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/finish" element={<Finish />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,39 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { useState } from "react";
+import { quesIDs } from "../data/quesIDs";
+import "./Home.css";
 
-export default Home
+const Home = () => {
+  const [username, setUsername] = useState<string>();
+  const [selQuesID, setSelQuesID] = useState<string[]>([]);
+  const [time, setTime] = useState(0);
+  const handleList = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setSelQuesID([...selQuesID, e.target.value]);
+      setTime((prev) => prev + 5);
+    } else {
+      setSelQuesID(selQuesID.filter((prop) => prop != e.target.value));
+      setTime((prev) => prev - 5);
+    }
+  };
+  return (
+    <main className="homeContainer">
+      <form className="">
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        {quesIDs.map((prop) => (
+          <div key={prop} className="" onChange={handleList}>
+            <label>{prop}</label>
+            <input name={prop} value={prop} type="checkbox" />
+          </div>
+        ))}
+        <button>Start Test</button>
+        <h4 className="">TIME: {time} min</h4>
+      </form>
+    </main>
+  );
+};
+
+export default Home;
